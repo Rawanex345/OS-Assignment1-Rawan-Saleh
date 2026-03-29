db88c394d8709688a8fa97b11fe9247cab0a1ce2
Rawanex345 <444052889@std.psau.edu.sa>
2026-03-29 22:00:32 +0300

    Update process.java

--- process.java
+++ process.java
@@ -5,12 +5,13 @@ public class Process implements Runnable {
     private int burstTime;
     private int timeQuantum;
     private int remainingTime;
-    private int priority;  
-    private long lastExecutionTime;
-// Feature 1: Priority field
+    private int priority;
+
+    // Waiting Time
     private long creationTime;
-private long totalWaitingTime;
-    
+    private long totalWaitingTime;
+    private long lastQueueEnterTime;
+
     // Constructor
     public Process(String name, int burstTime, int timeQuantum, int priority) {
         this.name = name;
@@ -18,35 +19,41 @@ private long totalWaitingTime;
         this.timeQuantum = timeQuantum;
         this.remainingTime = burstTime;
         this.priority = priority;
-         this.creationTime = System.currentTimeMillis(); 
-    this.totalWaitingTime = 0; 
-        this.lastExecutionTime = creationTime;
+
+        this.creationTime = System.currentTimeMillis();
+        this.totalWaitingTime = 0;
+        this.lastQueueEnterTime = creationTime;
     }
-    
+
     public void run() {
         int executionTime = Math.min(timeQuantum, remainingTime);
+
+
         long now = System.currentTimeMillis();
-totalWaitingTime += (now - lastExecutionTime);
+        totalWaitingTime += (now - lastQueueEnterTime);
+
         try {
             // Simulate execution
             Thread.sleep(executionTime);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
-        
+
         remainingTime -= executionTime;
-        
+
         if (remainingTime > 0) {
-            // Still has time left, will be re-queued
             System.out.println(name + " executed for " + executionTime + "ms, remaining: " + remainingTime + "ms");
         } else {
-            // Process finished
             System.out.println(name + " completed execution!");
-        } lastExecutionTime = System.currentTimeMillis();
+        }
     }
-   
+
     public void runToCompletion() {
         try {
+
+            long now = System.currentTimeMillis();
+            totalWaitingTime += (now - lastQueueEnterTime);
+
             Thread.sleep(remainingTime);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -54,36 +61,34 @@ totalWaitingTime += (now - lastExecutionTime);
         remainingTime = 0;
         System.out.println(name + " ran to completion (last process)!");
     }
+
+
+    public void enterQueue() {
+        lastQueueEnterTime = System.currentTimeMillis();
+    }
+
     
-    // Getters
     public String getName() {
         return name;
     }
-    
+
     public int getBurstTime() {
         return burstTime;
     }
-    
+
     public int getRemainingTime() {
         return remainingTime;
     }
-    
+
     public int getPriority() {
         return priority;
     }
-    
+
     public boolean isFinished() {
         return remainingTime <= 0;
     }
-    public long getCreationTime() {
-    return creationTime;
-}
 
-public void addWaitingTime(long time) {
-    this.totalWaitingTime += time;
-}
-
-public long getTotalWaitingTime() {
-    return totalWaitingTime;
-}
+    public long getTotalWaitingTime() {
+        return totalWaitingTime;
+    }
 }
